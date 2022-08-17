d15338e45c2ebc41f9492f0d9530e31eaa0a4410
Dwain06 <dwain.mobile@gmail.com>
2022-08-17 11:09:11 +0200

    fix (test jest): bug report date test jest fix

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -27,32 +27,42 @@ export default class {
     $('#modaleFile').modal('show')
   }
 
+
   getBills = () => {
+
     if (this.store) {
       return this.store
       .bills()
       .list()
       .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status)
-              }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
-          console.log('length', bills.length)
+        let bills = snapshot
+          // .sort(function(a,b){
+          //   return new Date(b.date) - new Date(a.date);
+          // });
+
+          // .sort((a, b) => ((a.date < b.date) ? 1 : -1))
+          console.log('bills', bills)
+          // bills = snapshot.map(doc => {
+          //   try {
+          //     return {
+          //       ...doc,
+          //       date: formatDate(doc.date),
+          //       status: formatStatus(doc.status)
+          //     }
+          //   } catch(e) {
+          //     // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+          //     // log the error and return unformatted date in that case
+          //     console.log(e,'for',doc)
+          //     return {
+          //       ...doc,
+          //       date: doc.date,
+          //       status: formatStatus(doc.status)
+          //     }
+          //   }
+          // })
+
+          // console.log('length', bills.length)
+          // console.log('bills', bills[0].date)
         return bills
       })
     }
